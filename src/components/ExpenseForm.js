0f4c76bf48ae44be92ba0ca06b989e7b80cf9544
Akash <aku.das02@gmail.com>
2021-01-17 23:30:27 +0600

    setup files

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExpenseForm = () => {
+  return (
+    <div>
+      <h2>Expense Form Component</h2>
+    </div>
+  );
+};
+
+export default ExpenseForm;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+
+const ExpenseList = () => {
+  return (
+    <div>
+      <h2>Expense List Component</h2>
+      <ExpenseItem />
+    </div>
+  );
+};
+
+export default ExpenseList;
