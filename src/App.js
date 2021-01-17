e56e36c6329d7ee66eae2afccc978a15684945ea
Akash <aku.das02@gmail.com>
2021-01-17 23:42:58 +0600

    initial list

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from "react";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 import Alert from "./components/Alert";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
+
+const initialExpenses = [
+  { id: uuidv4(), charge: "rent", amount: 1600 },
+  { id: uuidv4(), charge: "car payment", amount: 400 },
+  { id: uuidv4(), charge: "credit card bill", amount: 1200 },
+];
+
+console.log(initialExpenses);
 
 function App() {
   return (
