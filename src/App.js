925bb40a1a3629c983014b4842c317f10e8a5208
Akash <aku.das02@gmail.com>
2021-01-20 02:13:20 +0600

    localstorage API

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Alert from "./components/Alert";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
-const initialExpenses = [
-  { id: uuidv4(), charge: "rent", amount: 1600 },
-  { id: uuidv4(), charge: "car payment", amount: 400 },
-  { id: uuidv4(), charge: "credit card bill", amount: 1200 },
-];
+// const initialExpenses = [
+//   { id: uuidv4(), charge: "rent", amount: 1600 },
+//   { id: uuidv4(), charge: "car payment", amount: 400 },
+//   { id: uuidv4(), charge: "credit card bill", amount: 1200 },
+// ];
+
+const initialExpenses = localStorage.getItem("expenses")
+  ? JSON.parse(localStorage.getItem("expenses"))
+  : [];
 
 function App() {
   // ********* State Value **********
